aa87c87bfc2768063c22137995620e2e816c12ba
Diogo Miranda <diogosminfor@gmail.com>
2021-05-11 11:06:22 -0300

    add header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div>
+    <>
+      <Header />
       <GlobalStyle />
-      <h1>Olá mundo</h1>
-    </div>
+    </>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,9 +4,12 @@ export const GlobalStyle = createGlobalStyle`
 
 :root{
    
+    --white: #FFF;
     --red: #E52E4D;
     --blue: #5429CC;
     --blue-light: #6933FF;
+    --green: #33CC95;
+    --green-dark: #2DB381;
 
     --text-title: #363F5F;
     --text-body: #969CB3;
@@ -36,6 +39,15 @@ html{
 body{
     background: var(--background);
     --webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+
+h1, h2,h3,h4,h5,h6, strong{
+    font-weight: 600;
 }
 
 button{ 
