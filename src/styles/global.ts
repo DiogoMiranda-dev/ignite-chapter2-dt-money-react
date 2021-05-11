45c3c90a08c79b6512bfc1eed082559ab2482813
Diogo Miranda <diogosminfor@gmail.com>
2021-05-11 10:37:53 -0300

    add cores global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,17 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 
 :root{
+   
+    --red: #E52E4D;
+    --blue: #5429CC;
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
     --background: #f0f2f5;
+    --shape: #FFF;
+
 }
 
 *{
