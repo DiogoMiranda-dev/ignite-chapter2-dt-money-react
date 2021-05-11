b43550d7e98aef3fe2a8c14cca09a6f002ffbae6
Diogo Miranda <diogosminfor@gmail.com>
2021-05-11 16:38:20 -0300

    add api get transaction and db miragejs

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 import { Container, TransactionTypeContainer, RadioBox } from './styled';
 
 interface NewTransactionModalProps {
@@ -14,9 +15,24 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
 
+    const data = {
+        title, 
+        value,
+        category,
+        type,
+    }
+
+    api.post('transactions', data)
+
+  }
 
   return (
     <Modal
@@ -33,33 +49,51 @@ export function NewTransactionModal({
         <img src={closeImg} alt="Fechar modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
-        <input type="text" placeholder="Titulo" />
-        <input type="number" placeholder="Valor" />
+
+        <input
+          type="text"
+          placeholder="Titulo"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="value"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
-          <RadioBox 
+          <RadioBox
             type="button"
             onClick={() => setType('deposit')}
             isActive={type === 'deposit'}
             activeColor="green"
-            >
+          >
             <img src={incomeImg} alt="Entradas" />
             <span>Entrada</span>
           </RadioBox>
-          <RadioBox 
+          <RadioBox
             type="button"
             onClick={() => setType('withdraw')}
             isActive={type === 'withdraw'}
             activeColor="red"
-            >
+          >
             <img src={outcomeImg} alt="Saídas" />
             <span>Saídas</span>
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input type="text" placeholder="Categoria" />
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
+
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de seb site',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 6000,
+          createAt: new Date('2021-05-01 12:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Conta de Telefone',
+          type: 'withdraw',
+          category: 'Desenvolvimento',
+          amount: 175.2,
+          createAt: new Date('2021-05-10 12:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createAt: new Date()
-        }
-      ]
-    })
+      return this.schema.all('transactions');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transactions', data);
+    });
   },
 });
 
